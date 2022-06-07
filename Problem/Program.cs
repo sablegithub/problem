ff7fed2e1a6f7c0d5335d3e6997aba3476a09ffd
vaibhav sable <vaibhavsable52@gmail.com>
2022-06-06 20:47:59 -0700

    Prime Factorization

---
+++ Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(String[]args)
+    {
+        Console.WriteLine("Program form Prime Factor of Number");
+        
+        Console.WriteLine("Enter a Number");
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i<= x; i++)
+        {
+            if (x%i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
